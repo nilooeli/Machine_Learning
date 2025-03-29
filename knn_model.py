b3006a033106d37296f72aa652e4bdaac17c44b7
nilooeli <niloofar.eliyahoo@gmail.com>
2025-03-29 13:27:07 -0700

    Refactored notebook to use train_knn function from src/knn_model.py

--- knn_model.py
+++
@@ -1,6 +0,0 @@
-from sklearn.neighbors import KNeighborsClassifier
-
-def train_knn(X_train, y_train, n_neighbors=3):
-    model = KNeighborsClassifier(n_neighbors=n_neighbors)
-    model.fit(X_train, y_train)
-    return model
